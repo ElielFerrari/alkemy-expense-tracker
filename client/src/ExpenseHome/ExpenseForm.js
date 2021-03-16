0bbf5f29e385bcc3181946a55394f6790ea4b728
ElielFerrari <elielferrari1@gmail.com>
2021-03-16 18:04:17 -0300

    connect database, make expense form, and post petition

---
+++ client/src/ExpenseHome/ExpenseForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+
+function ExpenseForm () {
+
+    const [desc, setDesc] = useState(null);
+    const [amount, setAmount] = useState(null);
+    const [date, setDate] = useState(null);
+    const [type, setType] = useState(null);
+    const [category, setCategory] = useState(null);
+
+    const addTransaction = () => {
+        Axios.post("http://localhost:3001/create", {
+          desc,
+          category,
+          amount,
+          date,
+          type
+        }).then(()=>  console.log('Success'));
+      };
+
+    return(
+        <form autoComplete="off">
+            <label >Category:</label>
+            <input type="text" onChange={e => {setCategory(e.target.value)}} required />
+            <label >Description:</label>
+            <input type="text"  onChange={e => {setDesc(e.target.value)}} required/>
+            <label >Amount:</label>
+            <input type="number" onChange={e => {setAmount(e.target.value)}} required/>
+            <label >Type of Transaction:</label>
+            <input list="type" name="type" onChange={e => {setType(e.target.value)}} required></input>
+            <datalist id="type">
+                <option value="expense" />
+                <option value="deposit" />
+            </datalist>
+            <label >Date:</label>
+            <input type="date" onChange={e => {setDate(e.target.value)}} required />
+
+            <button type='submit' onClick={ addTransaction } >Add Transaction</button>
+        </form>
+    )
+};
+
+export default ExpenseForm;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
 
-const app = express()
-
-app.get('/', (req, res) => {
-    
-})
+app.use(cors());
+app.use(express.json());
 
 
-app.listen(3001, () => console.log('Server started on port 3000'))
+// Conexión a la db
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'expensesystem',
+});
+
+
+//Petición post de presupuestos a la db
+app.post('/create', (req, res) => {
+    const desc = req.body.desc;
+    const amount = req.body.amount;
+    const date = req.body.date;
+    const type = req.body.type;
+    const category = req.body.category;
+    const userId = req.body.userId;
+
+    db.query('INSERT INTO expense_table (description, amount, date, type, category, userID) VALUES (?,?,?,?,?,1)', [desc, amount, date, type, category, userId],
+     (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send('values inserted');
+        }
+    })
+});
+
+app.listen(3001, () => console.log('Server started on port 3001'));
